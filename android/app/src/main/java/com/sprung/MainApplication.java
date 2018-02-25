6b235523bac39ef6bb2a470455f3123e3462e95d
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-02-24 20:17:36 -0800

    Add RNGH

--- android/app/src/main/java/com/sprung/MainApplication.java
+++ android/app/src/main/java/com/sprung/MainApplication.java
@@ -3,6 +3,7 @@ package com.sprung;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -15,24 +16,24 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+      @Override
+      public boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+      }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage()
-      );
-    }
+      @Override
+      protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+                                           new VectorIconsPackage()
+                                           );
+      }
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+      @Override
+      protected String getJSMainModuleName() {
+        return "index";
+      }
+    };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
