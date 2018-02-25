16787ef45350222fcce58b382bf5ea6c84545d81
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-02-24 22:45:34 -0800

    [components] Add personalized orange tag

--- components/sprung-card.js
+++ components/sprung-card.js
@@ -5,17 +5,28 @@ import { Card, Title } from 'react-native-paper';
 import colors from '../product/colors';
 
 const styles = StyleSheet.create({
-  card: { padding: 5 },
-  kind_text: { color: colors.main_text_green, fontWeight: '700' },
+  card: { padding: 20 },
+  kind_text: { color: colors.main_text_green, fontWeight: '900', fontSize: 17 },
   activity_title: { fontSize: 22 },
   within_time: { color: 'grey' },
-  activity_top_row: { flexDirection: 'row' },
+  activity_top_row: { flexDirection: 'row', justifyContent: 'space-between' },
+  personalized: {
+    color: colors.with_focus_orange,
+    borderColor: colors.with_focus_orange,
+    borderWidth: 2,
+    paddingHorizontal: 8,
+    paddingVertical: 5,
+    fontWeight: '900',
+  },
 });
+
+const personalized = <Text style={styles.personalized}>Personalized</Text>;
 
 const ActivityColumn = ({ kind, title, within_time, is_personalized }) => (
   <View>
     <View style={styles.activity_top_row}>
       <Text style={styles.kind_text}>{kind}</Text>
+      {is_personalized === false ? null : personalized}
     </View>
     <Title style={styles.activity_title}>{title}</Title>
     <Text style={styles.within_time}>{within_time}</Text>
