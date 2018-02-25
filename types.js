19590104c54fd2797e3e1cd4a2f47edb3efd1383
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-02-24 21:33:23 -0800

    [root] Start types

---
+++ types.js
@@ -0,0 +1,9 @@
+export type cardData = {
+  kind: 'yoga' | 'dance' | 'meditation',
+  teacher_name: string,
+  teacher_image_uri: string,
+  title: string,
+  within_time: string,
+  is_personalized: boolean,
+  cost: string,
+};
