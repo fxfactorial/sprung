b7da6a30bc4824857a45199806c8b9eb95193da1
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-02-24 21:13:04 -0800

    [screens] Use components in fitness list

--- screens/available-fitness-list.js
+++ screens/available-fitness-list.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { View, StyleSheet, FlatList, Text } from 'react-native';
 
+import CalendarBar from '../components/calendar-bar';
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
+
 export default class FitnessList extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <CalendarBar />
         <Text>Hi</Text>
       </View>
     );
