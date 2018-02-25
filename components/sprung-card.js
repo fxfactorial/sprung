d8fabe0202b618cf4a4d5e9a3f0913a67dbd590b
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-02-24 22:59:11 -0800

    [components] Card style

--- components/sprung-card.js
+++ components/sprung-card.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Card, Title } from 'react-native-paper';
+import { Card, Title, Caption, Divider } from 'react-native-paper';
 
 import colors from '../product/colors';
 
 const styles = StyleSheet.create({
-  card: { padding: 20 },
+  card: { padding: 17 },
   kind_text: { color: colors.main_text_green, fontWeight: '900', fontSize: 17 },
-  activity_title: { fontSize: 22 },
-  within_time: { color: 'grey' },
-  activity_top_row: { flexDirection: 'row', justifyContent: 'space-between' },
+  act_title: { fontSize: 26, fontWeight: 'bold' },
+  act_top_row: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
   personalized: {
     color: colors.with_focus_orange,
     borderColor: colors.with_focus_orange,
     borderWidth: 2,
+    borderRadius: 5,
     paddingHorizontal: 8,
     paddingVertical: 5,
     fontWeight: '900',
@@ -24,14 +24,18 @@ const personalized = <Text style={styles.personalized}>Personalized</Text>;
 
 const ActivityColumn = ({ kind, title, within_time, is_personalized }) => (
   <View>
-    <View style={styles.activity_top_row}>
+    <View style={styles.act_top_row}>
       <Text style={styles.kind_text}>{kind}</Text>
       {is_personalized === false ? null : personalized}
     </View>
-    <Title style={styles.activity_title}>{title}</Title>
-    <Text style={styles.within_time}>{within_time}</Text>
+    <Title style={styles.act_title}>{title}</Title>
+    <Caption>{within_time}</Caption>
   </View>
 );
+
+// const Author = ({teacher_name, teacher_image_uri}) => (
+
+// )
 
 export default class SprungCard extends React.Component {
   render() {
