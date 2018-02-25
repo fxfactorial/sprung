f99bd3665607027040b1a9c46fae373bf9292e72
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-02-25 01:55:50 -0800

    [state][components] Expand calendar top row pick with FlatList

--- components/calendar-bar.js
+++ components/calendar-bar.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { Title } from 'react-native-paper';
+import { observer } from 'mobx-react/native';
+import format from 'date-fns/format';
+import isToday from 'date-fns/is_today';
 
 import application_styles from '../product/styles';
+import colors from '../product/colors';
+import { offerings_store } from '../state';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,20 +15,48 @@ const styles = StyleSheet.create({
     paddingLeft: '2%',
     paddingVertical: '2%',
   },
+  calendar_days_list: {
+    paddingVertical: 10,
+  },
+  contentContainerStyle: {
+    justifyContent: 'space-around',
+    width: '125%',
+  },
+  today_style: { alignItems: 'center' },
+  today_style_text: { fontSize: 20, fontWeight: 'bold' },
+  today_style_selected: { color: colors.with_focus_orange },
 });
 
-export default class CalendarSelectableBar extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Feburary</Text>
-        <View>
-          <View>
-            <Text>12</Text>
-            <Text>Monday</Text>
-          </View>
+export default observer(
+  class CalendarSelectableBar extends React.Component {
+    render_row({ item, index }) {
+      const day_as_num = format(item.scheduled_date, 'D');
+      const day_as_short_name = format(item.scheduled_date, 'ddd');
+      const s = isToday(item.scheduled_date)
+        ? [styles.today_style_text, styles.today_style_selected]
+        : [styles.today_style_text];
+      return (
+        <View style={styles.today_style}>
+          <Text style={s}>{day_as_num}</Text>
+          <Text style={s}>{day_as_short_name}</Text>
         </View>
-      </View>
-    );
+      );
+    }
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <Title>{offerings_store.current_month}</Title>
+          <FlatList
+            contentContainerStyle={styles.contentContainerStyle}
+            renderItem={this.render_row}
+            keyExtractor={({ scheduled_date }) => scheduled_date.toString()}
+            horizontal={true}
+            data={offerings_store.sprung_offerings}
+            style={styles.calendar_days_list}
+          />
+        </View>
+      );
+    }
   }
-}
+);
--- state/index.js
+++ state/index.js
@@ -1,4 +1,6 @@
 import { types, onSnapshot } from 'mobx-state-tree';
+import format from 'date-fns/format';
+import addDays from 'date-fns/add_days';
 
 const NavigationStoreModel = types
   .model('NavigationStore', {
@@ -12,8 +14,48 @@ const NavigationStoreModel = types
     },
   }));
 
+const Offering = types.model('Offering', {
+  id: types.identifier(),
+  cost: types.string,
+  author: types.string,
+});
+
+const OfferingsEachDay = types.model('OfferingEachDay', {
+  scheduled_date: types.Date,
+  offerings: types.array(Offering),
+});
+
+const SprungOfferings = types
+  .model('SprungOfferings', {
+    current_month: types.string,
+    sprung_offerings: types.array(OfferingsEachDay),
+  })
+  .actions(self => ({
+    refresh_offerings() {
+      //
+    },
+  }));
+
 export const nav_store = NavigationStoreModel.create({
   drawer_button_shows: true,
+});
+
+const TODAY = new Date();
+const MONTH = format(TODAY, 'MMMM');
+
+export const offerings_store = SprungOfferings.create({
+  current_month: MONTH,
+  sprung_offerings: [
+    { scheduled_date: TODAY, offerings: [{ id: '0', cost: '3.99', author: 'Jim Smith' }] },
+    {
+      scheduled_date: addDays(TODAY, 1),
+      offerings: [{ id: '1', cost: '3.99', author: 'Jim Smith' }],
+    },
+    {
+      scheduled_date: addDays(TODAY, 2),
+      offerings: [{ id: '2', cost: '3.99', author: 'Jim Smith' }],
+    },
+  ],
 });
 
 onSnapshot(nav_store, snapshot => {
