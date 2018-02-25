4309c4200809f5e66410f29816c13b74fe948f58
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-02-24 20:13:16 -0800

    Hold onto root navigation

---
+++ navigation/root-navigation.js
@@ -0,0 +1,6 @@
+import { StackNavigator } from 'react-navigation';
+import FitnessList from '../screens/available-fitness-list';
+
+export default StackNavigator({
+  'available-fitness-list': { screen: FitnessList },
+});
---
+++ screens/available-fitness-list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, FlatList, Text } from 'react-native';
+
+export default class FitnessList extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hi</Text>
+      </View>
+    );
+  }
+}
