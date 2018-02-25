9d346e7000ee98ecc146b5c57df04cb8507b5ca8
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-02-24 21:13:21 -0800

    [product] Bump fontWeight

--- product/styles.js
+++ product/styles.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  stack_navigator_header_text: { color: 'white', fontSize: 18 },
+  stack_navigator_header_text: { color: 'white', fontWeight: '700', fontSize: 22 },
 });
